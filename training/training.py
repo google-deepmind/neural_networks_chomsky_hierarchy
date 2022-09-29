2ea2be1109f1e0dcc35fdc8eb36e91a367cef46e
Gregoire Deletang <gdelt@google.com>
2022-09-29 02:28:15 -0700

    Update codebase with new tasks and features: - removing NDCF tasks, removing compare_occurrence, missing_palindrome and adding binary_multiplication, compute_sqrt, bucket_sort tasks, missing_duplicate tasks. - fixing bugs, accelerating positional encodings, and adding RoPE. - adding replace action in NDStack-RNNs, and removing unnormalized actions as we don't have log-space einsum as in PyTorch. - simplifying main task class, removing training/validation datasets and keeping sample_batch only. - dealing with random number perfect reproducibility (seeding of python.random, np.random, jax.random). - solving some bugs in range_evaluation. - implementing full auto-regressive transformer, with sampling at test time.
    PiperOrigin-RevId: 477670919
    Change-Id: Iccad3c475bf5f0073ebfcd5b1e1bcc4cfb911981

---
+++ training/training.py
@@ -0,0 +1,277 @@
+# Copyright 2022 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Training loop for base generalization experiments."""
+
+import dataclasses
+import functools
+import random
+from typing import Tuple, List, Callable, Mapping, Optional, Any
+
+import chex
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+import tqdm
+
+from neural_networks_chomsky_hierarchy.tasks import task as task_lib
+from neural_networks_chomsky_hierarchy.training import curriculum as curriculum_lib
+from neural_networks_chomsky_hierarchy.training import range_evaluation
+
+
+_LossMetrics = Optional[Mapping[str, jnp.ndarray]]
+_LossFn = Callable[[chex.Array, chex.Array], Tuple[float, _LossMetrics]]
+_AccuracyFn = Callable[[chex.Array, chex.Array], float]
+_ModelApplyFn = Callable[..., chex.Array]
+_MAX_RNGS_RESERVE = 50000
+
+
+@dataclasses.dataclass
+class ClassicTrainingParams:
+  """Parameters needed to train classical architectures."""
+  seed: int  # Used to sample during forward pass (e.g. from final logits).
+  model_init_seed: int  # Used to initialize model parameters.
+  training_steps: int
+  log_frequency: int
+
+  task: task_lib.GeneralizationTask
+  length_curriculum: curriculum_lib.Curriculum
+  batch_size: int
+
+  model: hk.Transformed
+  loss_fn: Callable[[jnp.ndarray, jnp.ndarray], Tuple[float, _LossMetrics]]
+  learning_rate: float
+  l2_weight: float
+  test_model: Optional[hk.Transformed] = None
+  max_grad_norm: float = 1.
+  is_autoregressive: bool = False
+
+  compute_full_range_test: bool = False
+  range_test_total_batch_size: int = 512
+  range_test_sub_batch_size: int = 64
+  max_range_test_length: int = 100
+
+  accuracy_fn: Optional[Callable[[jnp.ndarray, jnp.ndarray],
+                                 jnp.ndarray]] = None
+
+
+def _apply_loss_and_metrics_fn(
+    params: hk.Params,
+    rng_key: chex.PRNGKey,
+    batch: task_lib.Batch,
+    model_apply_fn: _ModelApplyFn,
+    loss_fn: _LossFn,
+    accuracy_fn: _AccuracyFn,
+    is_autoregressive: bool = False,
+) -> Tuple[float, Tuple[_LossMetrics, float]]:
+  """Computes the model output and applies the loss function.
+
+  Depending on whether a model is autoregressive or not, it will have a
+  different number of input parameters (i.e., autoregressive models also require
+  the targets as an input).
+
+  Args:
+    params: The model parameters.
+    rng_key: The prng key to use for random number generation.
+    batch: The data (consists of both inputs and outputs).
+    model_apply_fn: The model function that converts inputs into outputs.
+    loss_fn: A function that computes the loss for a batch of logits and labels.
+    accuracy_fn: A function that computes the accuracy for a batch of logits and
+      labels.
+    is_autoregressive: Whether the model is autoregressive or not.
+
+  Returns:
+    The loss of the model for the batch of data, extra loss metrics and the
+    accuracy, if accuracy_fn is not None.
+  """
+  if is_autoregressive:
+    outputs = model_apply_fn(
+        params, rng_key, batch["input"], batch["output"], sample=False)
+  else:
+    outputs = model_apply_fn(params, rng_key, batch["input"])
+
+  loss, loss_metrics = loss_fn(outputs, batch["output"])
+  if accuracy_fn is not None:
+    accuracy = accuracy_fn(outputs, batch["output"])
+  else:
+    accuracy = None
+  return loss, (loss_metrics, accuracy)
+
+
+@functools.partial(
+    jax.jit,
+    static_argnames=(
+        "model_apply_fn",
+        "loss_fn",
+        "accuracy_fn",
+        "optimizer",
+        "is_autoregressive",
+    ),
+)
+def _update_parameters(
+    params: hk.Params,
+    rng_key: chex.PRNGKey,
+    batch: task_lib.Batch,
+    model_apply_fn: _ModelApplyFn,
+    loss_fn: _LossFn,
+    accuracy_fn: _AccuracyFn,
+    optimizer: optax.GradientTransformation,
+    opt_state: optax.OptState,
+    is_autoregressive: bool = False,
+) -> Tuple[hk.Params, optax.OptState, Tuple[float, _LossMetrics, float]]:
+  """Applies a single SGD update step to the model parameters.
+
+  Args:
+    params: The model parameters.
+    rng_key: The prng key to use for random number generation.
+    batch: The data (consists of both inputs and outputs).
+    model_apply_fn: The model function that converts inputs into outputs.
+    loss_fn: A function that computes the loss for a batch of logits and labels.
+    accuracy_fn: A function that computes the accuracy for a batch of logits and
+      labels.
+    optimizer: The optimizer that computes the updates from the gradients of the
+      `loss_fn` with respect to the `params` and the previous `opt_state`.
+    opt_state: The optimizer state, e.g., momentum for each variable when using
+      Adam.
+    is_autoregressive: Whether the model is autoregressive or not.
+
+  Returns:
+    The updated parameters, the new optimizer state, and the loss, loss metrics
+    and accuracy.
+  """
+  (loss, (metrics, accuracy)), grads = jax.value_and_grad(
+      _apply_loss_and_metrics_fn,
+      has_aux=True)(params, rng_key, batch, model_apply_fn, loss_fn,
+                    accuracy_fn, is_autoregressive)
+  updates, new_opt_state = optimizer.update(grads, opt_state)
+  new_params = optax.apply_updates(params, updates)
+  return new_params, new_opt_state, (loss, metrics, accuracy)
+
+
+class TrainingWorker:
+  """Training worker."""
+
+  def __init__(self,
+               training_params: ClassicTrainingParams,
+               use_tqdm: bool = False):
+    """Initializes the worker.
+
+    Args:
+      training_params: The training parameters.
+      use_tqdm: Whether to add a progress bar to stdout.
+    """
+    self._training_params = training_params
+    self._use_tqdm = use_tqdm
+
+  def run(
+      self
+  ) -> Tuple[List[Mapping[str, Any]], Optional[List[Mapping[str, Any]]],
+             chex.ArrayTree]:
+    """Trains the model with the provided config.
+
+    Returns:
+      Results (various training and validation metrics), module parameters
+      and router parameters.
+    """
+    training_params = self._training_params
+    rngs_reserve = min(_MAX_RNGS_RESERVE, training_params.training_steps)
+
+    random.seed(training_params.seed)
+    np.random.seed(training_params.seed)
+    rng_seq = hk.PRNGSequence(training_params.seed)
+    rng_seq.reserve(rngs_reserve)
+
+    results = []
+    model = training_params.model
+    task = training_params.task
+    length_curriculum = training_params.length_curriculum
+
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(training_params.max_grad_norm),
+        optax.adam(training_params.learning_rate))
+
+    dummy_batch = task.sample_batch(
+        next(rng_seq), length=10, batch_size=training_params.batch_size)
+    model_init_rng_key = jax.random.PRNGKey(training_params.model_init_seed)
+
+    if training_params.is_autoregressive:
+      params = model.init(
+          model_init_rng_key,
+          dummy_batch["input"],
+          dummy_batch["output"],
+          sample=False)
+    else:
+      params = model.init(model_init_rng_key, dummy_batch["input"])
+
+    opt_state = optimizer.init(params)
+    self._params, self._step = params, 0
+
+    steps = range(training_params.training_steps + 1)
+    if self._use_tqdm:
+      steps = tqdm.tqdm(steps)
+    for step in steps:
+      # Randomness handled by either python.random or numpy.
+      length = length_curriculum.sample_sequence_length(step)
+      # Randomness handled by either jax, python.random or numpy.
+      train_batch = task.sample_batch(
+          next(rng_seq), length=length, batch_size=training_params.batch_size)
+      params, opt_state, (
+          train_loss, train_metrics, train_accuracy) = _update_parameters(
+              params=params,
+              rng_key=next(rng_seq),
+              batch=train_batch,
+              model_apply_fn=model.apply,
+              loss_fn=training_params.loss_fn,
+              accuracy_fn=training_params.accuracy_fn,
+              optimizer=optimizer,
+              opt_state=opt_state,
+              is_autoregressive=training_params.is_autoregressive)
+      self._params, self._step = params, step
+
+      log_freq = training_params.log_frequency
+      if (log_freq > 0) and (step % log_freq == 0):
+        log_data = {
+            "step": step,
+            "train_loss": float(train_loss),
+        }
+        if training_params.accuracy_fn is not None:
+          log_data["train_accuracy"] = float(train_accuracy)
+        for key, value in train_metrics.items():
+          log_data[".".join(["train_metrics", key])] = np.array(value)
+        results.append(log_data)
+
+      # We need to access this private attribute since the default reserve size
+      # can not be edited yet.
+      if not rng_seq._subkeys:  # pylint: disable=protected-access
+        rng_seq.reserve(rngs_reserve)
+
+    eval_results = None
+    if training_params.compute_full_range_test:
+      eval_params = range_evaluation.EvaluationParams(
+          model=training_params.test_model or model,
+          params=params,
+          accuracy_fn=training_params.accuracy_fn,
+          sample_batch=task.sample_batch,
+          max_test_length=training_params.max_range_test_length,
+          total_batch_size=training_params.range_test_total_batch_size,
+          sub_batch_size=training_params.range_test_sub_batch_size,
+          is_autoregressive=training_params.is_autoregressive,
+      )
+      eval_results = range_evaluation.range_evaluation(
+          eval_params, use_tqdm=False)
+
+    return results, eval_results, params
---
+++ training/utils.py
@@ -0,0 +1,196 @@
+# Copyright 2022 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Provides utility functions for training and evaluation."""
+
+import inspect
+from typing import Callable
+
+import chex
+from jax import lax
+from jax import nn as jnn
+from jax import numpy as jnp
+
+from neural_networks_chomsky_hierarchy.tasks import task
+
+COMPUTATION_EMPTY_TOKEN = 0
+OUTPUT_EMPTY_TOKEN = 1
+DELIMITER_TOKEN = 2
+
+
+def pad_input_tape(
+    input_tape: chex.Array,
+    generalization_task: task.GeneralizationTask,
+    use_delimiters: bool = False,
+    computation_steps_mult: int = 0,
+) -> chex.Array:
+  """Pads the input tape to account for the task's output length.
+
+  For a given input tape `input_tape` of vocabulary size `vocab_sizes`, the
+  padded tape will have the format [`delimiter_symbol`, `input_tape`,
+  `empty_tape`, `delimiter_symbol`], where the empty tape corresponds to token
+  `vocab_size + 1` and the `delimiter_symbol` corresponds to token `vocab_size +
+  2`. The `empty_tape` has the same length as the task output.
+
+  Args:
+    input_tape: Input tokens of shape `(batch_size, input_length, input_size)`.
+    generalization_task: The task that we train on.
+    use_delimiters: Whether to use delimiter symbols on the tape.
+    computation_steps_mult: The amount of empty cells to append to the input
+      tape. This variable is a multiplier and the actual number of cell is
+      computation_steps_mult * input_length.
+
+  Returns:
+    The padded sequence.
+  """
+  batch_size, input_length, input_size = input_tape.shape
+  output_length = generalization_task.output_length(input_length)
+  extra_dims_onehot = 3 if use_delimiters else 2
+
+  # Adding extra zeros to the inputs to let space for the extra tokens.
+  input_tape = jnp.concatenate(
+      [input_tape,
+       jnp.zeros((batch_size, input_length, extra_dims_onehot))],
+      axis=-1)
+
+  if computation_steps_mult > 0:
+    # We create the portion of the tape used for computation. It contains a
+    # first type of empty symbols.
+    computation_tape = jnp.full(
+        (batch_size, computation_steps_mult * input_length),
+        fill_value=input_size + COMPUTATION_EMPTY_TOKEN)
+    computation_tape = jnn.one_hot(
+        computation_tape, num_classes=input_size + extra_dims_onehot)
+    input_tape = jnp.concatenate([input_tape, computation_tape], axis=1)
+
+  # Here we create the portion of the tape used to retrieve the output after
+  # computation. It contains a second type of empty symbols.
+  output_tape = jnp.full((batch_size, output_length),
+                         fill_value=input_size + OUTPUT_EMPTY_TOKEN)
+  output_tape = jnn.one_hot(
+      output_tape, num_classes=input_size + extra_dims_onehot)
+  input_tape = jnp.concatenate([input_tape, output_tape], axis=1)
+
+  if use_delimiters:
+    # We add the delimiter symbol to the beginning and the end of the tape.
+    delimiter_symbol = jnp.full((batch_size, 1),
+                                fill_value=input_size + DELIMITER_TOKEN)
+    delimiter_symbol = jnn.one_hot(
+        delimiter_symbol, num_classes=input_size + extra_dims_onehot)
+    input_tape = jnp.concatenate(
+        [delimiter_symbol, input_tape, delimiter_symbol], axis=1)
+  return input_tape
+
+
+def wrap_model_with_pad(
+    model: Callable[..., chex.Array],
+    generalization_task: task.GeneralizationTask,
+    use_delimiters: bool = False,
+    computation_steps_mult: int = 0,
+    single_output: bool = False,
+    is_autoregressive: bool = False,
+) -> Callable[..., chex.Array]:
+  """Returns a wrapped model that pads the inputs to match the output length.
+
+  For a given input tape `input_tape` of vocabulary size `vocab_size`, the
+  wrapped model will process a tape of the format
+  [`delimiter_symbol`, `input_tape`, `empty_tape`, `delimiter_symbol`], where
+  the empty tape token is `vocab_size + 1` and the `delimiter_symbol` token is
+  `vocab_size + 2`. The `empty_tape` has the same length as the task output.
+
+  Args:
+    model: A model function that converts inputs to outputs.
+    generalization_task: The task that we train on.
+    use_delimiters: Whether to use delimiter symbols on the tape.
+    computation_steps_mult: The amount of empty cells to append to the input
+      tape. This variable is a multiplier and the actual number of cells is
+      `computation_steps_mult * input_length`.
+    single_output: Whether to return the squeezed tensor of values.
+    is_autoregressive: Whether the model is autoregressive or not.
+  """
+
+  def new_model(x: chex.Array) -> chex.Array:
+    output_length = generalization_task.output_length(x.shape[1])
+    input_tape = pad_input_tape(x, generalization_task, use_delimiters,
+                                computation_steps_mult)
+
+    if 'input_length' in inspect.getfullargspec(model).args:
+      output = model(input_tape, input_length=x.shape[1])
+    else:
+      output = model(input_tape)
+
+    if use_delimiters:
+      output = output[:, -output_length - 1:-1, :]
+    output = output[:, -output_length:, :]
+
+    if single_output:
+      output = jnp.squeeze(output, axis=1)
+    return output
+
+  def new_model_autoregressive(
+      x: chex.Array,
+      y: chex.Array,
+      sample: bool,
+  ) -> chex.Array:
+    """Returns an autoregressive model if `sample == True and output_size > 1`.
+
+    Args:
+      x: The input sequences of shape (b, t, i), where i is the input size.
+      y: The target sequences of shape (b, t, o), where o is the output size.
+      sample: Whether to evaluate the model using autoregressive decoding.
+    """
+    output_length = generalization_task.output_length(x.shape[1])
+
+    if not sample or output_length == 1:
+      output = model(x, y)
+
+    else:
+
+      def evaluate_model_autoregressively(
+          idx: int,
+          predictions: chex.Array,
+      ) -> chex.Array:
+        """Iteratively evaluates the model based on the previous predictions.
+
+        Args:
+          idx: The index of the target sequence that should be evaluated.
+          predictions: The logits for the predictions up to but not including
+            the index `idx`.
+
+        Returns:
+          The `predictions` array modified only at position `idx` where the
+          logits for index `idx` have been inserted.
+        """
+        one_hot_predictions = jnn.one_hot(
+            jnp.argmax(predictions, axis=-1),
+            num_classes=generalization_task.output_size,
+        )
+        logits = model(x, one_hot_predictions)
+        return predictions.at[:, idx].set(logits[:, idx])
+
+      output = lax.fori_loop(
+          lower=0,
+          upper=output_length,
+          body_fun=evaluate_model_autoregressively,
+          init_val=jnp.empty_like(y))
+
+    if single_output:
+      output = jnp.squeeze(output, axis=1)
+    return output
+
+  if is_autoregressive:
+    return new_model_autoregressive
+
+  return new_model
